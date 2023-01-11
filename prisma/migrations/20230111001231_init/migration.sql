3601ae90d6329d3dc6708c8874cf9273a901faf2
Filippo Di Marco <filidm5795@gmail.com>
2023-01-11 01:29:03 +0100

    add prisma

---
+++ prisma/migrations/20230111001231_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Prisma" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "lastname" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Prisma_pkey" PRIMARY KEY ("id")
+);
