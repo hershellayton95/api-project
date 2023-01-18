c84de6c31915d987c3e7e55f869a6005a568a6ba
Filippo Di Marco <filidm5795@gmail.com>
2023-01-18 17:37:00 +0100

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,16 @@
 import express from "express";
 import "express-async-errors";
-import cors from "cors";
 import planetsRoutes from "./routes/planets"
 
-import { validationMiddlewareError } from
-    "./lib/middleware/validation";
+import { validationMiddlewareError } from "./lib/middleware/validation";
 
+import { initCorsMiddleware } from "./lib/middleware/cors";
 
-
-const corsOptions = {
-    origin: "http://localhost:8080",
-};
 
 const app = express();
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(initCorsMiddleware())
 
 app.use("/planets", planetsRoutes)
 
